eb67b826140e0d78409e790dd20809d252e8d282
Pat <pat_boo61@tbs.tu.ac.th>
2024-05-31 10:17:40 +0700

    Complete exercise for castle and secret room levels

--- src/components/01_Castle.jsx
+++ src/components/01_Castle.jsx
@@ -1,5 +1,14 @@
-const Castle = () => {
-  return <div>Castle</div>;
+import SecretRoom from "./09_SecretRoom";
+
+const Castle = ({message1, onReply}) => {
+  
+  return (
+    <div>
+      <h2>Castle Room</h2>
+      <p>Message for JSD7: {message1}</p>
+      <SecretRoom message1={message1} onReply={onReply} />
+    </div>
+  );
 };
 
 export default Castle;
--- src/components/09_SecretRoom.jsx
+++ src/components/09_SecretRoom.jsx
@@ -1,5 +1,24 @@
-const SecretRoom = () => {
-  return <div>SecretRoom</div>;
+import React, {useState} from "react";
+
+const SecretRoom = ({message1, onReply}) => {
+  const [reply, setReply] = useState("");
+
+  function sendReply() {
+    onReply(reply);
+  }
+
+  function handleChange({target}) {
+    setReply(target.value);
+  }
+  return (
+    <>
+      <h2>SecretRoom</h2>
+      <p>Message for JSD7: {message1}</p>
+
+      <input type="text" onChange={handleChange}/>
+      <button onClick={sendReply}>SUBMIT</button>
+    </>
+  );
 };
 
 export default SecretRoom;
